02e58151758af4bd2b579069f07d154f621b6812
El-Bongo <koppeks.contactos@gmail.com>
2023-01-25 00:39:20 -0300

    detalles del fondo

--- src/components/FrontPage/FrontPage.jsx
+++ src/components/FrontPage/FrontPage.jsx
@@ -1,4 +1,14 @@
 
+import squares from "../../images/ninesquares.svg"
+import wiggle from "../../images/wiggle.svg"
+import wiggle2 from "../../images/wiggle2.svg"
+import space from "../../images/space.svg"
+import space1 from "../../images/space1.svg"
+import succeed from "../../images/succeed.svg"
+import objective from "../../images/objective.svg"
+import contact from "../../images/contact.svg"
+import chatting from "../../images/chatting.svg"
+
 function FrontPage(){
 
     return(
@@ -29,6 +39,34 @@ function FrontPage(){
                     <li><p>HTML5</p></li>
                 </ul>
             </div>
+            </div>
+            <div>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <span className="detail"></span>
+                <span className="detail"></span>
+                <img className="detail" src={wiggle} alt="detail wiggle background"/>
+                <img className="detail" src={wiggle} alt="detail wiggle background"/>
+                <img className="detail" src={wiggle} alt="detail wiggle background"/>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <img className="detail" src={wiggle} alt="detail wiggle background"/>
+                {/* 10 */}
+                <span className="detail"></span>
+                <img className="detail" src={space} alt="detail space background"/>
+                <img className="detail" src={space1} alt="detail space background"/>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <img className="detail" src={succeed} alt="detail succeed background"/>
+                <span className="detail"></span>
+                <img className="detail" src={objective} alt="detail objective background"/>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <img className="detail" src={wiggle} alt="detail wiggle background"/>
+                <img className="detail" src={wiggle2} alt="detail wiggle background"/>
+                {/* 20 */}
+                <img className="detail" src={contact} alt="detail contact background"/>
+                <img className="detail" src={chatting} alt="detail chatting background"/>
+                <img className="detail" src={squares} alt="detail square background"/>
+                <img className="detail" src={wiggle} alt="detail wiggle background"/>
             </div>
         </>
     )
--- src/components/Porfolio/Porfolio.jsx
+++ src/components/Porfolio/Porfolio.jsx
@@ -1,7 +1,6 @@
-// import service from "../../images/services.jpg"
-// import ecomm from "../../images/ecomm.jpg"
-// import code from "../../images/code.png"
 
+import squares from "../../images/ninesquares.svg"
+import wiggle from "../../images/wiggle.svg"
 
 function Porfolio(){
 
@@ -48,48 +47,6 @@ function Porfolio(){
                     </div>
                 </div>
             </div>
-
-            {/* <div className='porfolio_cards'>
-                <div className='porfolio_card'>
-                    <div className='porfolio_imagedet'>
-                        <section></section>
-                        <div className='porfolio_imgzoomeffect'>
-                            <img src={service} alt="" />
-                        </div>
-                    </div>
-                    <div className='porfolio_info'>
-                        <h3>titulo de prueba</h3>
-                        <p>texto random de prueba holaaaa</p>
-                    </div>
-                </div>
-                <div className='porfolio_card'>
-                    <div className='porfolio_imagedet'>
-                        <section></section>
-                        <div className='porfolio_imgzoomeffect'>
-                            <img src={code} alt="" />
-                        </div>
-                    </div>
-                    <div className='porfolio_info'>
-                        <h3>titulo de prueba</h3>
-                        <p>texto random de prueba holaaaa</p>
-                    </div>
-                </div>
-                <div className='porfolio_card'>
-                    <div className='porfolio_imagedet'>
-                        <section></section>
-                        <div className='porfolio_imgzoomeffect'>
-                            <img src={ecomm} alt="" />
-                        </div>
-                    </div>
-                    <div className='porfolio_info'>
-                        <h3>titulo de prueba</h3>
-                        <p>texto random de prueba holaaaa</p>
-                    </div>
-                </div>
-            </div>
-            <div className='porfolio_seemore'>
-                <a href="#">Ver más ...</a> 
-            </div> */}
         </div>
     )
 }
