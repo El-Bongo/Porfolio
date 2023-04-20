161688a298b9de9ffd2e3d9e1b2f0a877545a8c8
El-Bongo <koppeks.contactos@gmail.com>
2023-04-20 18:28:37 -0300

    add experience

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,96 +1,105 @@
-import React from "react"
+import React from "react";
 
-import { Formik } from "formik"
+import { Formik } from "formik";
 import { basicSchema } from "../../schemas";
 import FormInput from "../FormInputs/FormInput";
-import emailjs from "@emailjs/browser"
+import emailjs from "@emailjs/browser";
 import Footer from "../Footer/Footer";
 import { Trans, useTranslation } from "react-i18next";
 import i18next from "i18next";
 
-function ContactMe(){
+function ContactMe() {
+  const [t, i18n] = useTranslation();
 
-    const [t, i18n] = useTranslation()
+  return (
+    <div className="form_main section" id="sectionContact">
+      <h2>
+        <Trans i18nKey="contact.title">Contacta conmigo</Trans>
+      </h2>
+      <Formik
+        initialValues={{ name: "", email: "", reason: "", message: "" }}
+        validationSchema={basicSchema}
+        onSubmit={(values, { resetForm }) => {
+          const data = {
+            name: values.name,
+            email: values.email,
+            reason: values.reason,
+            message: values.message,
+          };
 
-    return(
-        <div className="form_main section" id="sectionContact">
-            <h2>
-                <Trans i18nKey="contact.title">
-                    Contacta conmigo
-                </Trans>
-            </h2>
-            <Formik
-                initialValues={{name: '', email: '', reason: '', message: ''}}
-                validationSchema={basicSchema}
-                onSubmit={(values, {resetForm}) =>{
+          emailjs.sendForm(
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
+            "#form",
+            process.env.REACT_APP_PUBLIC_KEY
+          );
 
-                    const data = {
-                        name: values.name,
-                        email: values.email,
-                        reason: values.reason,
-                        message: values.message
-                    }
+          resetForm();
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => {
+          const { name, email, reason, message } = values;
 
-                    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, "#form", process.env.REACT_APP_PUBLIC_KEY)
-
-                    resetForm()
-                    
-                }}
-            >
-
-            {({values, errors, touched, handleChange, handleBlur, handleSubmit}) =>{
-          
-            const {name, email, reason, message} = values
-
-            return(
-                <form className="form_container" id="form">
-                    <FormInput 
-                        value={name}
-                        name="name"
-                        error={touched.name && errors.name}
-                        placeholder="contact.form.placeholder1"
-                        label="contact.form.name1"
-                        onChange={handleChange('name')}
-                        onBlur={handleBlur("name")}
-                    />
-                    <FormInput 
-                        value={email}
-                        name="email"
-                        error={touched.email && errors.email}
-                        placeholder="contact.form.placeholder2"
-                        label="Email"
-                        onChange={handleChange('email')}
-                        onBlur={handleBlur("email")}
-                    />
-                    <FormInput 
-                        value={reason}
-                        name="reason"
-                        error={touched.reason && errors.reason}
-                        placeholder="contact.form.placeholder3"
-                        label="contact.form.name3"
-                        onChange={handleChange('reason')}
-                        onBlur={handleBlur("reason")}
-                    />
-                    <FormInput 
-                        value={message}
-                        name="message"
-                        error={touched.message && errors.message}
-                        placeholder="contact.form.placeholder4"
-                        label="contact.form.name4"
-                        onChange={handleChange('message')}
-                        onBlur={handleBlur("message")}
-                    />
-                    {
-                        Object.keys(errors).length > 0 ? 
-                        <button className="button" disabled>{i18next.t("contact.form.button")}</button>
-                        :
-                        <button className="button" onClick={handleSubmit}>{i18next.t("contact.form.button")}</button>
-                    }
-                </form>
-            )}}
-            </Formik>
-        </div>
-    )
+          return (
+            <form className="form_container" id="form">
+              <FormInput
+                value={name}
+                name="name"
+                error={touched.name && errors.name}
+                placeholder="contact.form.placeholder1"
+                label="contact.form.name1"
+                onChange={handleChange("name")}
+                onBlur={handleBlur("name")}
+              />
+              <FormInput
+                value={email}
+                name="email"
+                error={touched.email && errors.email}
+                placeholder="contact.form.placeholder2"
+                label="Email"
+                onChange={handleChange("email")}
+                onBlur={handleBlur("email")}
+              />
+              <FormInput
+                value={reason}
+                name="reason"
+                error={touched.reason && errors.reason}
+                placeholder="contact.form.placeholder3"
+                label="contact.form.name3"
+                onChange={handleChange("reason")}
+                onBlur={handleBlur("reason")}
+              />
+              <FormInput
+                value={message}
+                name="message"
+                error={touched.message && errors.message}
+                placeholder="contact.form.placeholder4"
+                label="contact.form.name4"
+                onChange={handleChange("message")}
+                onBlur={handleBlur("message")}
+              />
+              {Object.keys(errors).length > 0 ? (
+                <button className="button" disabled>
+                  {i18next.t("contact.form.button")}
+                </button>
+              ) : (
+                <button className="button" onClick={handleSubmit}>
+                  {i18next.t("contact.form.button")}
+                </button>
+              )}
+            </form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
 }
 
-export default ContactMe
+export default ContactMe;
--- src/components/Porfolio/Porfolio.jsx
+++ src/components/Porfolio/Porfolio.jsx
@@ -1,73 +1,120 @@
-
 import { Trans, useTranslation } from "react-i18next";
-import squares from "../../images/ninesquares.svg"
-import wiggle from "../../images/wiggle.svg"
+import squares from "../../images/ninesquares.svg";
+import wiggle from "../../images/wiggle.svg";
 
-function Porfolio(){
+function Porfolio() {
+  const [t, i18n] = useTranslation();
 
-    const [t, i18n] = useTranslation()
+  function handleShow(e) {
+    const projects = document.querySelectorAll(".project");
+    projects.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("id") === e.target.id) {
+        link.classList.add("active");
+      }
+    });
+  }
 
-    function handleShow(e) {
-
-        const projects = document.querySelectorAll('.project');
-        projects.forEach((link) => {
-          link.classList.remove('active');
-          if(link.getAttribute('id') === e.target.id){
-            link.classList.add('active');
-          }
-        });
-        
-    }
-
-    return(
-        <div className='porfolio_container section' id="sectionPorfolio">
-            <h2>Porfolio</h2>
-            <div className="porfolio_projects">
-                <div className="projects_name">
-                    <h3 onClick={handleShow} id="1" >MateWeb</h3>
-                    <h3 onClick={handleShow} id="2" >SinnelReparaciones</h3>
-                    <h3 onClick={handleShow} id="3" >E-commerce SoyHenry</h3>
-                </div>
-                <div className="projects_text">
-                    <div id="1" className="project">
-                        <p>
-                            <Trans i18nKey="porfolio.mateweb">
-                                Un proyecto personal, que era para ofrecer mis servicios en internet, en ese momento solo sabia JavaScript, HTML y CSS.
-                            </Trans>
-                        </p>
-                        <div className="project_links">
-                            <a href="https://github.com/Koppeks/Mateweb" target="_blank" rel="noopener noreferrer">Github</a>
-                        </div>
-                    </div>
-                    <div id="2" className="project">
-                        <p>
-                            <Trans i18nKey="porfolio.sinnel">
-                            Era un local de reparaciones de computadoras, necesitaba una pagina para tener mas visibilidad y aumentar el flujo de clientes.
-                            </Trans>
-                        </p>
-                        <div className="project_links">
-                            <a href="https://github.com/Koppeks/sinnelreparaciones" target="_blank" rel="noopener noreferrer">Github</a>
-                        </div>
-                    </div>
-                    <div id="3" className="project">
-                        <p >
-                            <Trans i18nKey="porfolio.soyhenry">
-                                El proyecto final de el bootcamp de SoyHenry, un e-commerce de venta de componentes de pc, con API de MercadoPago, MetaMask y Auth0.
-                            </Trans>
-                        </p>
-                        <div className="project_links">
-                            <a href="https://ecommerce-frontend-30b.vercel.app/" target="_blank" rel="noopener noreferrer">
-                                <Trans i18nKey="porfolio.link">
-                                    Ver el sitio
-                                </Trans>
-                            </a>
-                            <a href="https://github.com/Koppeks/Ecommerce-Frontend-30b" target="_blank" rel="noopener noreferrer">Github</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="porfolio_container section" id="sectionPorfolio">
+      <h2>Porfolio</h2>
+      <div className="porfolio_projects">
+        <div className="projects_name">
+          <h3 onClick={handleShow} id="1">
+            MateWeb
+          </h3>
+          <h3 onClick={handleShow} id="2">
+            SinnelReparaciones
+          </h3>
+          <h3 onClick={handleShow} id="3">
+            E-commerce SoyHenry
+          </h3>
+          <h3 onClick={handleShow} id="4">
+            Alan Urri porfolio
+          </h3>
         </div>
-    )
+        <div className="projects_text">
+          <div id="1" className="project">
+            <p>
+              <Trans i18nKey="porfolio.mateweb">
+                Un proyecto personal, que era para ofrecer mis servicios en
+                internet, en ese momento solo sabia JavaScript, HTML y CSS.
+              </Trans>
+            </p>
+            <div className="project_links">
+              <a
+                href="https://github.com/Koppeks/Mateweb"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+          <div id="2" className="project">
+            <p>
+              <Trans i18nKey="porfolio.sinnel">
+                Era un local de reparaciones de computadoras, necesitaba una
+                pagina para tener mas visibilidad y aumentar el flujo de
+                clientes.
+              </Trans>
+            </p>
+            <div className="project_links">
+              <a
+                href="https://github.com/Koppeks/sinnelreparaciones"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+          <div id="3" className="project">
+            <p>
+              <Trans i18nKey="porfolio.soyhenry">
+                El proyecto final de el bootcamp de SoyHenry, un e-commerce de
+                venta de componentes de pc, con API de MercadoPago, MetaMask y
+                Auth0.
+              </Trans>
+            </p>
+            <div className="project_links">
+              <a
+                href="https://ecommerce-frontend-30b.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Trans i18nKey="porfolio.link">Ver el sitio</Trans>
+              </a>
+              <a
+                href="https://github.com/Koppeks/Ecommerce-Frontend-30b"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+          <div id="4" className="project">
+            <p>
+              <Trans i18nKey="porfolio.alan">
+                Una Landing page para un profesional del marketing y contenido
+                digital.
+              </Trans>
+            </p>
+            <div className="project_links">
+              <a
+                href="https://alan-porfolio.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Trans i18nKey="porfolio.link">Ver el sitio</Trans>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Porfolio
+export default Porfolio;
